546ba5bcb8decf706f8701111da6221fb8f3b8ff
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-06-26 02:32:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -9,13 +9,4 @@ reset();
 //Gets Freddy nose honk sound effect
 var x = document.getElementById("myAudio"); 
 
-// Stuff for the score    
-function playAudio() { 
-x.pause;
-x.currentTime = 0;
-x.play();
-count = count + 1;
-document.getElementById("score").innerHTML = count;
-}
-
     
